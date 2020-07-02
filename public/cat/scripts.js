856785bb667c3ccfee7ddad9483a38fff19fec67
lucas dalessandro <lucas.dalessandro@gmail.com>
2020-07-02 14:53:18 -0400

    q1

--- public/cat/scripts.js
+++ public/cat/scripts.js
@@ -1,1 +1,36 @@
-// here there be JS, yarrr ☠️
+const conversationElem = document.querySelector("#conversation-container");
+const messageInput = document.querySelector("#user-input");
+const handleFocus = () => {
+  messageInput.focus();
+};
+const sendMessage = (event) => {
+  // prevent the default "page reload" from occurring.
+  event.preventDefault();
+  const message = { author: "user", text: messageInput.value };
+  updateConversation(message);
+
+  fetch("/cat-message")
+    .then((res) => res.json())
+    .then((data) => {
+      updateConversation(data.message);
+    });
+};
+
+const updateConversation = (message) => {
+  // deconstruct the message object
+  const { author, text } = message;
+  // create a <p> element
+  const messageElem = document.createElement("p");
+  // add a 'message' class and a class based on the author
+  messageElem.classList.add("message", author);
+  // add the text message to the element
+  messageElem.innerHTML = `<span>${text}</span>`;
+  // append the element to the conversation
+  conversationElem.appendChild(messageElem);
+  handleFocus();
+  if (author === "user") {
+    messageInput.value = "";
+  }
+};
+
+handleFocus();
